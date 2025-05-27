0ffe77440e186fa7f8acc56f986788c42c40f88a
enerzilius <efblouback@gmail.com>
2025-05-27 00:08:57 -0300

    abrindo primeira janela!

--- Main.cpp
+++ Main.cpp
@@ -2,31 +2,59 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
+using namespace std;
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void resize(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void render(GLFWwindow* window);
+
+
+int main() {
 	glfwInit();
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "HelloWindow", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
-	if (!window) {
+	glfwSetFramebufferSizeCallback(window, resize);
+
+	if (window == NULL) {
 		std::cout << "Falha na criação da janela" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
+	gladLoadGL();
 
-	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, resize);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+}
+
+void resize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void render(GLFWwindow* window) {
+
+	// here we put our rendering code
+
 }
