51f2cd5bfe136fc204a1ad4568aa6d3989f697df
enerzilius <efblouback@gmail.com>
2025-05-28 20:41:57 -0300

    hello triangle

--- Main.cpp
+++ Main.cpp
@@ -19,10 +19,9 @@ const char* vertexShaderSource = "#version 330 core\n"
 	"}\0";
 
 const char* fragShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
 	"out vec4 FragColor;\n"
 	"void main(){\n"
-	"	FragColor = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+	"	FragColor = vec4(0.8f, 0.8f, 0.0f, 1.0f);\n"
 	"}\0";
 
 
@@ -55,12 +54,6 @@ int main() {
 	 0.5f, -0.5f, 0.0f,
 	 0.0f,  0.5f, 0.0f
 	};
-
-	//buffer para enviar os vértices para a GPU de uma vez
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -113,10 +106,38 @@ int main() {
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+	
 
+	// 0. copy our vertices array in a buffer for OpenGL to use
+	//buffer para enviar os vértices para a GPU de uma vez
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 1. then set the vertex attributes pointers
+	//define como o programa deve ler cada atributo
+		// 0 designa qual atributo do vertex configurar 
+		// 3 designa o tamanho do vertice (vec3)
+		// GL_FLOAT é o tipo do dado
+		// opção de normalizar os dados (só faz efeito em ints
+		// delimita o espaço entre os vértices
+		// offset
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
-	//tudo após isso utiliza o programa para renderização
-	glUseProgram(shaderProgram);
+	//armazena as chamadas de atributos do vértice, facilitando o desenho de formatoss (já que é só trocar o VAO)
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	// ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 
 	//loop de renderização
 	while (!glfwWindowShouldClose(window))
@@ -127,6 +148,10 @@ int main() {
 		//aqui vai os processos de renderização
 		glClearColor(0.7f, 0.3f, 0.7f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//checar por eventos e trocar os buffers
 		glfwPollEvents();
