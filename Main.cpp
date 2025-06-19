d5a50d68cfa3f7fbd80293a94edc3ef07d9b908a
enerzilius <efblouback@gmail.com>
2025-06-18 23:39:50 -0300

    aplicando textura somente na parte com a != 0 e glm

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 #include "shaderClass.h"
 #include "VBO.h"
