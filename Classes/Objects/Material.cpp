7c7cc7cd93856b8ff17421412149b40a7b688320
enerzilius <efblouback@gmail.com>
2025-10-11 00:10:45 -0300

    tirando prints

--- Classes/Objects/Material.cpp
+++ Classes/Objects/Material.cpp
@@ -2,20 +2,16 @@
 
 #include <glm/glm.hpp>
 #include <stb/stb_image.h>
-#include <string>
-#include <iostream>
 #include "../glAbstractions/Texture.h"
 #include "../Utils/GeneralUtilities.h"
 
 using namespace std;
 
 Material::Material(glm::vec3 ambientColor, glm::vec3 diffuseColor, glm::vec3 specularColor, float shininessValue, const char* texturePath) {
-	cout << ambientColor.x << diffuseColor.x << specularColor.x << shininessValue << texturePath << std::endl;
 	ambient = ambientColor;
 	diffuse = diffuseColor;
 	specular = specularColor;
 	shininess = shininessValue;
-	cout << texturePath << endl;
 	if (texturePath && std::strlen(texturePath) > 0) applyTexture(texturePath);
 }
 
@@ -24,7 +20,6 @@ void Material::applyTexture(const char* texturePath) {
 	texture.ActiveTexture(GL_TEXTURE0);
 	texture.Bind();
 	texture.SetTexParameters();
-	std::cout << "aaaa";
 	int width, height, ch;
 	stbi_set_flip_vertically_on_load(false);
 	unsigned char* data = stbi_load(texturePath, &width, &height, &ch, 0);
