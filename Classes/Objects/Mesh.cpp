e76f989b11e04aae2f3dcc4f707c5023e7609f1e
enerzilius <efblouback@gmail.com>
2025-11-04 14:32:07 -0300

    setup mesh

--- Classes/Objects/Mesh.cpp
+++ Classes/Objects/Mesh.cpp
@@ -20,6 +20,17 @@ Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture
 
 void Mesh::setupMesh() {
     this->vao = new VAO();
-    this->vbo = new VBO(getFloatArrayFromVertexArray(vertices.data()), sizeof(vertices.data()));
+    this->vbo = new VBO(&vertices[0].Normal.x, sizeof(vertices.data()));
     this->ebo = new EBO(indices.data(), sizeof(indices));
+
+    vao->Bind();
+
+    int layoutVertex = 0, layoutUV = 1, layoutNormal = 2;
+    int stepVertex = 3, stepUV = 2, stepNormal = 3;
+    int stride = stepVertex + stepUV + stepNormal;
+    vao->LinkVBO(*vbo, layoutVertex, stepVertex, stride, 0);
+    vao->LinkVBO(*vbo, layoutNormal, stepNormal, stride, stepVertex);
+    vao->LinkVBO(*vbo, layoutUV, stepUV, stride, stepVertex+stepNormal);
+
+    vao->Unbind();
 }
